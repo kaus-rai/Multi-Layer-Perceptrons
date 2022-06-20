1567dd48fd941bf4b708e83178ae9cb690e18490
komekez <kausr2595@gmail.com>
2022-06-20 09:55:47 -0700

    Learning Rate and Metrics Defined

--- main.py
+++ main.py
@@ -35,3 +35,43 @@ learning_rate = 0.001
 regularizer_rate = 0.1
 
 #Defining Placeholders
+input_X = tf.placeholder('float32', shape=(None, features), name='input_X')
+input_Y = tf.placeholder('float32', shape=(None, classes), name='input_Y')
+prob = tf.placeholder(tf.float32)
+
+#Initializing the weights and bias by normal function with std=1/sqrt(number of input features)
+weights_0 = tf.Variable(tf.random_normal([features, layer_0], stddev=(1/tf.sqrt(float(features)))))
+bias_0 = tf.Variable(tf.random_normal([layer_0]))
+
+weights_1 = tf.Variable(tf.random_normal([layer_0, layer_1], stddev=(1/tf.sqrt(float(layer_0)))))
+bias_1 = tf.Variable(tf.random_normal([layer_1]))
+
+weights_2 = tf.Variable(tf.random_normal([layer_1, output], stddev=(1/tf.sqrt(float(layer_1)))))
+bias_2 = tf.Variable(tf.random_normal([output]))
+
+
+#Defining model 
+#784(Input)-512(Hidden layer 1)-256(Hidden layer 2)-10(Output) model
+hidden_output_0 = tf.nn.relu(tf.matmul(input_X, weights_0) + bias_0)
+hidden_output_0_0 = tf.nn.dropout(hidden_output_0, prob)
+
+hidden_output_1 = tf.nn.relu(tf.matmul(input_X, weights_1) + bias_1)
+hidden_output_1_1 = tf.nn.dropout(hidden_output_1, prob)
+
+pred_y = tf.sigmoid(tf.matmul(hidden_output_1_1, weights_2) + bias_2)
+
+#Defining Loss Function using Softmax cross Entropy
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = pred_y, labels=input_Y))+regularizer_rate*(tf.reduce_sum(tf.square(bias_0))+tf.reduce_sum(tf.square(bias_1)))
+
+#Learning Rate
+learning_rate = tf.train.exponential_decay(learning_rate, 0, 5, 0.85, staircase=True)
+
+#Adam Optimizer for finding the true weight
+optmizer = tf.train.AdamOptimizer(learning_rate).minimize(loss, var_list=[weights_0, weights_1,weights_2,bias_0,bias_1,bias_2])
+
+#Metrics Definition
+correct_pred = tf.equal(tf.argmax(y_train, 1), tf.argmax(pred_y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+#Training Parameters
