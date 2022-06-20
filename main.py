4c9b38edc1bc1f637f4d3dc8eb377162dbe7e309
komekez <kausr2595@gmail.com>
2022-06-20 09:34:32 -0700

    Parameters Defined

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from datasetHelper import loadDataset, changeDimensionality, hotEncoding
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import roc_auc_score, accuracy_score
+
+sess = tf.InteractiveSession()
 
 #Image height and width
 img_h, img_w = 28, 28
@@ -19,3 +24,14 @@ y_test = hotEncoding(y_test)
 
 print("Train Label Dimensions", y_train.shape)
 print("Test Label Dimensions", y_test.shape)
+
+#Defining Parameters
+classes = y_train.shape[1]
+features = X_train.shape[1]
+output = y_train.shape[1]
+layer_0 = 512
+layer_1 = 256
+learning_rate = 0.001
+regularizer_rate = 0.1
+
+#Defining Placeholders
